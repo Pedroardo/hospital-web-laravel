e17823b3b77c35be7e443f3933e2eb15aaacde0c
Pedro <pedroardo10@gmail.com>
2025-09-18 09:58:24 +0700

    add query to get data in the specialist  and crud

--- be/app/Repositories/SpecialistRepository.php
+++ be/app/Repositories/SpecialistRepository.php
@@ -2,7 +2,44 @@
 
 namespace App\Repositories;
 
+use App\Models\Specialist;
+
 class SpecialistRepository{
 
+    public function getAll(array $fields){
+        // Query optimization agar tidak mengambil semua data dan menggunakan lazy loading serta mengambil data 10 dari kemungkinan ada 1000 data dengan paginate
+        return Specialist::select($fields)->latest()->with(['hospitals', 'doctors'])->paginate(10);
+    }
+
+    public function getById(int $id, array $fields){
+
+        // Query function untuk mengambil data specialist berdasarkan id
+        return Specialist::select($fields)->
+        with(['hospitals'=> function($q) use($id) {
+            $q->withCount(['doctors as doctors_count' => function($query) use ($id) {
+                $query->where('specialist_id', $id);
+            }]);
+        },
+        'doctors' => function($q) use ($id) {
+            $q->where('specialist_id', $id)->with('hospital:id,name,city,post_code');
+        }
+    ])->findOrFail($id);
+
+    }
+
+    public function create(array $data){
+        return Specialist::create($data);
+    }
+
+    public function update(int $id, array $data){
+        $specialist = Specialist::findOrFail($id);
+        $specialist->update($data);
+        return $specialist;
+    }
+
+    public function delete(int $id){
+        $specialist = Specialist::findOrFail($id);
+        $specialist->delete();
+    }
     
 }
